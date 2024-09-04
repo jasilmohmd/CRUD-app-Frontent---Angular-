529b8db1be0026cfd058e889cf5bdc5d604f0c93
jasilmohmd <jasilcheruppa@gmail.com>
2024-09-04 10:59:14 +0530

    modal fix

--- src/app/component/admin/dashboard/dashboard.component.ts
+++ src/app/component/admin/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Router, RouterLink, RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { User } from '../../../Model/UserCredentials';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { AdminAuthService } from '../../../services/admin-auth.service';
@@ -14,38 +14,34 @@ import { map, Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { FormsModule } from '@angular/forms';
 
-
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule,RouterLink,FontAwesomeModule,DeleteModalComponent,AsyncPipe,FormsModule],
+  imports: [CommonModule, RouterLink, FontAwesomeModule, DeleteModalComponent, AsyncPipe, FormsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class AdminDashboardComponent implements OnInit{
-
+export class AdminDashboardComponent implements OnInit {
 
   faTrashAlt = faTrashAlt;
   faPencilAlt = faPencilAlt;
 
   activeModal: number | null = null; // Track the active modal
 
-  users!: Observable<User[]>
-  error!: Observable<string | null>
+  users!: Observable<User[]>;
+  error!: Observable<string | null>;
 
-  filteredUsers: User[] = []; // Filtered users to be displayed
+  filteredUsers$: Observable<User[]> = new Observable(); // Filtered users to be displayed
   searchQuery: string = ''; // Search query input
 
   adminAuthService = inject(AdminAuthService);
   route = inject(ActivatedRoute);
 
-  constructor( private http:HttpClient, private router:Router, private store: Store){
-
+  constructor(private http: HttpClient, private router: Router, private store: Store) {
     const token = localStorage.getItem('admin') ?? '';
-    this.store.dispatch(AdminActions.loadUsers({token}));
+    this.store.dispatch(AdminActions.loadUsers({ token }));
     this.users = this.store.select(AdminSelectors.selectAllUsers);
     this.error = this.store.select(AdminSelectors.selectUsersError);
-    
   }
 
   ngOnInit(): void {
@@ -53,8 +49,8 @@ export class AdminDashboardComponent implements OnInit{
   }
 
   filterUsers(): void {
-    this.users = this.store.select(AdminSelectors.selectAllUsers).pipe(
-      map(users => 
+    this.filteredUsers$ = this.users.pipe(
+      map(users =>
         users.filter(user =>
           user.name.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
           user.email.toLowerCase().includes(this.searchQuery.toLowerCase())
@@ -64,18 +60,13 @@ export class AdminDashboardComponent implements OnInit{
   }
 
   openModal(index: number, event: Event): void {
-    
     event.stopPropagation();
     this.activeModal = index; // Set the active modal index
     console.log(this.activeModal);
   }
 
   closeModal(): void {
-    const token: string = localStorage.getItem('admin') ?? '';
-
-    this.activeModal=null; // Close the modal
-    this.adminAuthService.getUsers(token).subscribe((res:any)=> { this.users = res.users })
+    this.activeModal = null; // Close the modal
+    this.filterUsers(); // Reapply the filter to update the displayed users
   }
-
-
 }
